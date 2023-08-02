67d94298e1ea125a6ef1d25cb0263f37c2b97716
bebenlebricolo <benoittarrade@hotmail.fr>
2023-08-02 19:14:53 +0700

    Updated how the Ui is populated when user account is retrieved
    Signed-off-by: bebenlebricolo <benoittarrade@hotmail.fr>

--- DruidsCornerApp/Views/Login/GoogleSignInPage.xaml.cs
+++ DruidsCornerApp/Views/Login/GoogleSignInPage.xaml.cs
@@ -19,57 +19,75 @@ public partial class GoogleSignInPage : ContentPage
         // do something here
         if (e.account != null)
         {
+            var imageFrame = new Frame()
+            {
+                Content = new Image()
+                {
+                    Source = ImageSource.FromUri(new Uri(e.account.PhotoUrl!)),
+                    HeightRequest = 80,
+                    WidthRequest = 80
+                },
+                CornerRadius = 15,
+                Padding = new Thickness(0,0),
+                Margin = new Thickness(0,0),
+                IsClippedToBounds = true,
+                Background = null,
+                BorderColor = Colors.Transparent,
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Start,
+            };
+
+            var nameLabel = new Label()
+            {
+                Text = e.account.FullName,
+                FontSize = 20,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            var emailLabel = new Label()
+            {
+                Text = e.account.Email,
+                FontSize = 10,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            var labelStackLayout = new VerticalStackLayout()
+            {
+                Children =
+                {
+                    nameLabel,
+                    emailLabel
+                },
+                HorizontalOptions = LayoutOptions.Fill,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            var grid = new Grid()
+            {
+                ColumnDefinitions = new ColumnDefinitionCollection(new[]
+                                                                   {
+                                                                       new ColumnDefinition(GridLength.Auto),
+                                                                       new ColumnDefinition(GridLength.Star)
+                                                                   }
+                                                                  ),
+                ColumnSpacing = 30,
+                Margin = new Thickness(0,0),
+                Padding = new Thickness(0,0)
+            };
+            
+            // Adding the elements inside
+            grid.Add(imageFrame, 0,0);
+            grid.Add(labelStackLayout, 1,0);
+            
+            // Put everyone into a nice frame
             var buttonFrame = new Frame()
             {
-                CornerRadius = 20,
+                CornerRadius = 10,
                 BackgroundColor = Colors.White,
                 BorderColor = Colors.DeepSkyBlue,
-                Content = new Grid()
-                {
-                    ColumnDefinitions = new ColumnDefinitionCollection(new[]
-                                                                       {
-                                                                           new ColumnDefinition(GridLength.Auto),
-                                                                           new ColumnDefinition(GridLength.Star)
-                                                                       }
-                                                                      ),
-                    ColumnSpacing = 10,
-                    Children =
-                    {
-                        // Top left
-                        new Frame()
-                        {
-                            Content = new Image()
-                            {
-                                Source = ImageSource.FromUri(new Uri(e.account.PhotoUrl!))
-                            },
-                            CornerRadius = 30,
-                            IsClippedToBounds = true,
-                            Background = null,
-                            BorderColor = Colors.Transparent,
-                            VerticalOptions = LayoutOptions.Center,
-                            HorizontalOptions = LayoutOptions.Start
-                        },
-                        new VerticalStackLayout()
-                        {
-                            Children =
-                            {
-                                new Label()
-                                {
-                                    Text = e.account.FullName,
-                                    FontSize = 20,
-                                    VerticalOptions = LayoutOptions.Center
-                                },
-                                new Label()
-                                {
-                                    Text = e.account.Email,
-                                    FontSize = 10,
-                                    VerticalOptions = LayoutOptions.Center
-                                }
-                            },
-                            HorizontalOptions = LayoutOptions.Fill
-                        }
-                    }
-                }
+                Content = grid,
+                Margin = new Thickness(0,0),
+                Padding = new Thickness(10,10)
             };
 
             BaseVerticalStackLayout.Add(buttonFrame);

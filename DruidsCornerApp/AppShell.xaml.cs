e2536c05bea95eb164c34ca43c8d25a94e97a0a9
bebenlebricolo <benoittarrade@hotmail.fr>
2023-09-27 15:38:40 +0700

    Trying flyouts and tabs
    Signed-off-by: bebenlebricolo <benoittarrade@hotmail.fr>

--- DruidsCornerApp/AppShell.xaml.cs
+++ DruidsCornerApp/AppShell.xaml.cs
@@ -15,21 +15,27 @@ public partial class AppShell : Shell
 	public AppShell(ISecureStorageService storageService)
 	{
 		InitializeComponent();
-		Routing.RegisterRoute(Navigator.GetWelcomePageRoute(), typeof(WelcomePage));
-		Routing.RegisterRoute(Navigator.GetBasicSignInPageRoute(), typeof(BasicSignInPage));
-		Routing.RegisterRoute(Navigator.GetAccountCreationPageRoute(), typeof(AccountCreationPage));
-		Routing.RegisterRoute(Navigator.GetGoogleSignInPageRoute(), typeof(GoogleSignInPage));
-		Routing.RegisterRoute(Navigator.GetAccountPasswordResetPageRoute(), typeof(ResetPasswordPage));
-		
-		// Recipe-related pages
-		Routing.RegisterRoute(Navigator.GetRecipesBrowserPageRoute(), typeof(RecipeExplorerPage));
+		// Routing.RegisterRoute(Navigator.GetWelcomePageRoute(), typeof(WelcomePage));
+		// Routing.RegisterRoute(Navigator.GetBasicSignInPageRoute(), typeof(BasicSignInPage));
+		// Routing.RegisterRoute(Navigator.GetAccountCreationPageRoute(), typeof(AccountCreationPage));
+		// Routing.RegisterRoute(Navigator.GetGoogleSignInPageRoute(), typeof(GoogleSignInPage));
+		// Routing.RegisterRoute(Navigator.GetAccountPasswordResetPageRoute(), typeof(ResetPasswordPage));
+		//
+		// // Recipe-related pages
+		// Routing.RegisterRoute(Navigator.GetRecipesBrowserPageRoute(), typeof(RecipeExplorerPage));
 
 		_storageService = storageService;
-		var firstStart = Task.Run(async () => await _storageService.GetAsync(GlobalApplicationStates.AppFirstBootKey)).Result;
-		if(firstStart != null  && !bool.Parse(firstStart))
+	}
+
+	protected override async void OnAppearing()
+	{
+		var firstStart = await _storageService.GetAsync(GlobalApplicationStates.AppFirstBootKey);
+		if(firstStart == null  || bool.Parse(firstStart))
 		{
 			// Now we can skip this check next time
-			Task.Run(async () => await _storageService.StoreAsync(GlobalApplicationStates.AppFirstBootKey, true.ToString()));
+			await _storageService.StoreAsync(GlobalApplicationStates.AppFirstBootKey, false.ToString());
 		}
+		//await GoToAsync(Navigator.GetRecipesBrowserPageRoute());
+		await GoToAsync("///Explore");
 	}
 }

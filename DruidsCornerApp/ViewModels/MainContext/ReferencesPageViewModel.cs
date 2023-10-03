4ae8734b1ba01844f2f4a467200f006fc63e605f
bebenlebricolo <benoittarrade@hotmail.fr>
2023-10-03 12:28:02 +0700

    Trying to get scroll views to work with sharpnado tabs
    Signed-off-by: bebenlebricolo <benoittarrade@hotmail.fr>

--- DruidsCornerApp/ViewModels/MainContext/ReferencesPageViewModel.cs
+++ DruidsCornerApp/ViewModels/MainContext/ReferencesPageViewModel.cs
@@ -1,6 +1,7 @@
 using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using DruidsCornerApiClient.Models.RecipeDb;
 using DruidsCornerApiClient.Services;
 using DruidsCornerApp.Models.Login;
 using DruidsCornerApp.Models.MainContext;
@@ -14,6 +15,8 @@ public partial class ReferencesPageViewModel : BaseViewModel
 {
     private readonly ILogger<RecipeExplorerViewModel> _logger;
 
+    public ObservableCollection<CompactHopModel> Hops { get; private set; } = new();
+    
     [ObservableProperty]
     private int _selectedViewModelIndex = 0;
 
@@ -25,5 +28,74 @@ public partial class ReferencesPageViewModel : BaseViewModel
     ) : base("Recipes browser", false)
     {
         _logger = logger;
+        InitFakeHops();
+    }
+    
+    private void InitFakeHops()
+    {
+        Hops.Clear();
+        Hops.Add(new CompactHopModel
+        {
+            Name = "Magnum US",
+            Purpose = "Bittering",
+            Rating = 3.5,
+            AlphaAcids = "10 - 16 %",
+            Favorite = false,
+            StockedAmount = 153
+        });
+        Hops.Add(new CompactHopModel
+        {
+            Name = "Magnum GR",
+            Purpose = "Bittering",
+            Rating = 3.7,
+            AlphaAcids = "12 - 16 %",
+            Favorite = true,
+            StockedAmount = 0
+        });
+        Hops.Add(new CompactHopModel
+        {
+            Name = "Cascade",
+            Purpose = "Hybrid",
+            Rating = 3.8,
+            AlphaAcids = "4.5 - 9 %",
+            Favorite = true,
+            StockedAmount = 86
+        });
+
+        Hops.Add(new CompactHopModel
+        {
+            Name = "Chinook",
+            Purpose = "Hybrid",
+            Rating = 3.8,
+            AlphaAcids = "11.5 - 15 %",
+            Favorite = true,
+            StockedAmount = 33
+        });
+
+        Hops.Add(new CompactHopModel
+        {
+            Name = "Saaz",
+            Purpose = "Aromatic",
+            Rating = 3.2,
+            AlphaAcids = "2 - 5 %",
+            Favorite = false,
+            StockedAmount = 0
+        });
+
+        Hops.Add(new CompactHopModel
+        {
+            Name = "Hallertau Blanc",
+            Purpose = "Aromatic",
+            Rating = 4.1,
+            AlphaAcids = "9 - 12 %",
+            Favorite = true,
+            StockedAmount = 0
+        });
+    }
+
+    [RelayCommand]
+    public async Task RefreshData(CancellationToken cancellationToken)
+    {
+        InitFakeHops();
     }
 }
--- DruidsCornerApp/Views/MainContext/HopReferenceView.xaml.cs
+++ DruidsCornerApp/Views/MainContext/HopReferenceView.xaml.cs
@@ -1,19 +1,104 @@
-using DruidsCornerApp.ViewModels.MainContext;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.Input;
+using DruidsCornerApp.Models.MainContext;
 
 namespace DruidsCornerApp.Views.MainContext;
 
 public partial class HopReferenceView : ContentView
 {
-    private readonly HopReferenceViewModel _viewModel;
-    
+    public ObservableCollection<CompactHopModel> Hops { get; } = new()
+    {
+        new CompactHopModel
+        {
+            Name = "Magnum US",
+            Purpose = "Bittering",
+            Rating = 3.5,
+            AlphaAcids = "10 - 16 %",
+            Favorite = false,
+            StockedAmount = 153
+        }
+    };
+
     public HopReferenceView()
     {
         InitializeComponent();
-        
+        InitFakeHops();
+    }
+
+    private void InitFakeHops()
+    {
+        Hops.Clear();
+        Hops.Add(new CompactHopModel
+        {
+            Name = "Magnum US",
+            Purpose = "Bittering",
+            Rating = 3.5,
+            AlphaAcids = "10 - 16 %",
+            Favorite = false,
+            StockedAmount = 153
+        });
+        Hops.Add(new CompactHopModel
+        {
+            Name = "Magnum GR",
+            Purpose = "Bittering",
+            Rating = 3.7,
+            AlphaAcids = "12 - 16 %",
+            Favorite = true,
+            StockedAmount = 0
+        });
+        Hops.Add(new CompactHopModel
+        {
+            Name = "Cascade",
+            Purpose = "Hybrid",
+            Rating = 3.8,
+            AlphaAcids = "4.5 - 9 %",
+            Favorite = true,
+            StockedAmount = 86
+        });
+
+        Hops.Add(new CompactHopModel
+        {
+            Name = "Chinook",
+            Purpose = "Hybrid",
+            Rating = 3.8,
+            AlphaAcids = "11.5 - 15 %",
+            Favorite = true,
+            StockedAmount = 33
+        });
+
+        Hops.Add(new CompactHopModel
+        {
+            Name = "Saaz",
+            Purpose = "Aromatic",
+            Rating = 3.2,
+            AlphaAcids = "2 - 5 %",
+            Favorite = false,
+            StockedAmount = 0
+        });
+
+        Hops.Add(new CompactHopModel
+        {
+            Name = "Hallertau Blanc",
+            Purpose = "Aromatic",
+            Rating = 4.1,
+            AlphaAcids = "9 - 12 %",
+            Favorite = true,
+            StockedAmount = 0
+        });
     }
     
-    public HopReferenceView(HopReferenceViewModel viewModel) : this()
+    private void Button_OnClicked(object? sender, EventArgs e)
     {
-        BindingContext = viewModel;
+       InitFakeHops();
+    }
+
+    private void ScrollView_OnScrolled(object? sender, ScrolledEventArgs e)
+    {
+        Console.WriteLine($"ScrollX: {e.ScrollX}, ScrollY: {e.ScrollY}");
+    }
+
+    private void ItemsView_OnScrolled(object? sender, ItemsViewScrolledEventArgs e)
+    {
+        Console.WriteLine($"ScrollX: {e.HorizontalDelta}, ScrollY: {e.VerticalDelta}");
     }
 }
